b70cdf2e864c22892b78e6388aa94b008028beda
kapilgangwani007 <kapilgangwani007@gmail.com>
2022-04-05 02:48:18 +0530

    Create kapil.py
    functions defined

---
+++ CA_ONE-10584241/kapil.py
@@ -0,0 +1,26 @@
+class Employee :
+
+   def __init__(self,StaffID, LastName, FirstName, RegHours, HourlyRate, OTMultiple, TaxCredit, StandardBand):
+    self.StaffID = StaffID
+    self.LastName = LastName
+    self.FirstName = FirstName
+    self.RegHours = RegHours
+    self.HourlyRate = HourlyRate
+    self.OTMultiple = OTMultiple
+    self.TaxCredit = TaxCredit
+    self.StandardBand = StandardBand
+
+    def Computepayment(self, HoursWorked, date):
+      self.HoursWorked = HoursWorked
+      self.date = date
+
+
+    def Calculate():
+
+    def testNetLessEqualGross(self):
+        
+       
+
+
+
+
