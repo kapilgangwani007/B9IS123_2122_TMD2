7fba269444acd79709fe17bfc5bfc7ab4460e0de
kapilgangwani007 <kapilgangwani007@gmail.com>
2022-04-09 08:10:53 +0530

    Calculation logic implemented

--- CA_ONE-10584241/Employee.py
+++ CA_ONE-10584241/Employee.py
@@ -1,45 +1,90 @@
-class Employee :
+# To validate the output of each test cases, importing unittest framework.
+import unittest
 
-   def __init__(self,StaffID, LastName, FirstName, RegHours, HourlyRate, OTMultiple, TaxCredit, StandardBand):
-    self.StaffID = StaffID
-    self.LastName = LastName
-    self.FirstName = FirstName
-    self.RegHours = RegHours
-    self.HourlyRate = HourlyRate
-    self.OTMultiple = OTMultiple
-    self.TaxCredit = TaxCredit
-    self.StandardBand = StandardBand
+# Defining the class Employee.
+class Employee:
 
-    def Computepayment(self, HoursWorked, date):
-      self.HoursWorked = HoursWorked
-      self.date = date
+    # Initialize to the data members of the class using constructor.
+    def _init_(self, Staff_id, first_name, last_name, reg_hours, hourly_rate, ot_multiple, tax_credit, standard_band):
+        # To access the variable in globally in class, defining self.
+        # initialising the variable
+        self.Staff_id = Staff_id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.reg_hours = reg_hours
+        self.hourly_rate = hourly_rate
+        self.ot_multiple = ot_multiple
+        self.tax_credit = tax_credit
+        self.standard_band = standard_band
 
+    # defining a method computePayment by passing parameters of hours_worked and date
+    def computePayment(self, hours_worked, date):
+        self.hours_worked = hours_worked
+        self.date = date
+        # declaring a dictionatary
+        emp_info = {}
 
-    def Calculate():
+        self.regular_rate = self.hourlyRate
+        self.standard_rate_pay = self.standard_band  # if self.grossPay > self.standardBand else 0
 
- /*
-Worked hours = Entered by User (42)
-Regular Worked Hours = if(RegHours)RegHours (37)
-Overtime Hours Worked = if(Worked hours > RegHours) ? RegHours - Worked hours: Worked hours; (5)
-Regular Rate = Hourlyrate (16)
-Overtime Rate = if((Worked hours > RegHours) ? Hourlyrate+(OTMultiple*Overtime Hours Worked) : 0) 16+(1.5*5) = 23.5
-Regular Pay = RegHours*Hourlyrate (37*16 = 592)
-Overtime Pay = if((Worked hours > RegHours) ? Overtime Rate*Overtime Hours Worked : 0; (23.5*5) = 117.5 
-Gross Pay = Regular Pay + Overtime Pay (592+117.5 = 709.5)
-Standard Rate Pay = StandardBand
-Higher Rate Pay = if(Gross Pay>Standard Rate Pay) ? Gross Pay - Standard Rate Pay : 0 ; 712-710 = 2 
-Standard Tax = 20% of Standard Rate Pay (710*.20 = 142)
-Higher Tax = 40% of Higher Rate Pay (2*.40 = 0.8)
-Total Tax = Standard Tax + Total Tax (142.80)
-Tax Credit = TaxCredit (72)
-Net Tax = Total Tax - TaxCredit(142.80 - 72 = 70.8)
-PRSI = 4% of Net Tax (70.8*.04 = 28.32)
-Net Deductions = Net Tax + Net Deductions = 70.8+28.32= 99.12
-Net Pay = Gross Pay - Net Deductions = 712 - 99.12 = 612.88
-*/
-        
-       
+        if self.reg_hours > self.hours_worked:
+        else:
+            self.overtime_hours_worked = 0
+
+        if self.overtime_hours_worked != 0 and self.hours_worked > self.reg_hours:
+            self.overtime_rate = round(self.regular_rate + (self.ot_multiple * self.overtime_hours_worked),2)
+        else:
+            self.overtime_rate = 0
+
+        self.overtime_pay = self.overtime_rate * self.overtime_hours_worked if self.hoursWorked > self.regHours else 0
 
 
+        if self.overtime_rate != 0 and self.hours_worked > self.reg_hours:
+            self.overtime_pay = self.overtime_rate * self.overtime_hours_worked
+        else:
+            self.overtime_pay = 0
 
+        #self.overtime_pay = self.overtime_rate * self.overtime_hours_worked if self.hours_worked > self.reg_hours else 0
 
+        self.gross_pay = self.regular_pay + self.overtime_pay
+
+        if self.gross_pay > self.standard_band:
+            self.higher_rate_pay = self.gross_pay - self.standard_rate_pay
+        else:
+            self.higher_rate_pay = 0
+
+        self.standard_tax = round(self.standard_rate_pay * 0.20,2)
+        self.higher_tax = round(self.higher_rate_pay * 0.40,2)
+        self.prsi = round(self.gross_pay * 0.04, 2)
+        self.total_tax = self.standard_tax + self.higher_tax
+
+        if self.total_tax > self.tax_credit:
+            self.net_tax = round(self.total_tax - self.tax_credit,2)
+        else:
+            self.net_tax = self.total_tax
+
+        self.net_deductions = round(self.net_tax + self.prsi, 2)
+        self.net_pay = round(self.gross_pay - self.net_deductions, 2)
+
+        emp_info["Staff Id"] = self.Staff_id
+        emp_info["Last Name"] = self.last_name
+        emp_info["First Name"] = self.first_name
+        emp_info['Regular Hours Worked'] = self.reg_hours
+        emp_info["Overtime Hours Worked"] = self.overtime_hours_worked
+        emp_info["Regular Rate"] = self.hourly_rate
+        emp_info["Overtime Rate"] = self.overtime_rate
+        emp_info["Regular Pay"] = self.regular_pay
+        emp_info["Overtime Pay"] = self.overtime_pay
+        emp_info["Gross Pay"] = self.gross_pay
+        emp_info["Standard Rate Pay"] = self.standard_band
+        emp_info["Higher Rate Pay"] = self.higher_rate_pay
+        emp_info["Standard Tax"] = self.standard_tax
+        emp_info["Higher Tax"] = self.higher_tax
+        emp_info["Total Tax"] = self.total_tax
+        emp_info["Tax Credit"] = self.tax_credit
+        emp_info["Net Tax"] = self.net_tax
+        emp_info["PRSI"] = self.prsi
+        emp_info["Net Deductions"] = self.net_deductions
+        emp_info["Net Pay"] = self.net_pay
+        print(emp_info)
+        return emp_info
